c5a796786f75f0dcef49c8fc2188872dd75c2e9f
Priya <priyamcanandha@gmail.com>
2026-05-06 21:31:40 +0530

    Initial Commit

---
+++ chapter_01_basics/01_JS_Basics.js
@@ -0,0 +1,4 @@
+console.log("Hello Basic of JS!");
+
+let x = 10;
+console.log(x);
---
+++ chapter_01_basics/03_JS_Commands.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+
+//MAC - Darwin
+//Windows - win32
+//Linux - linux
+
+console.log(process.arch);  
+//x64 - 64 bit
+//ia32 - 32 bit 
+
+console.log("Node Version: " + process.version);
