30fa76783c08b94011897df315972e8b590f885f
Priya <priyamcanandha@gmail.com>
2026-05-18 10:01:55 +0530

    Add chapter 6 operators examples

---
+++ chapter_06_operators/39_logical_operator.js
@@ -0,0 +1,11 @@
+// && -> AND gate
+// || -> OR gate
+
+let a = true;
+let b = false;
+
+console.log(a && b); // false
+console.log(a || b); // true
+console.log(!a); // false
+
+console.log(5 != "g"); // true
---
+++ chapter_06_operators/44_null_operator.js
@@ -0,0 +1,10 @@
+console.log(null == 0); // false, == operator will not coerce the null.
+console.log(null >= 0); // true, bcz relational operators like >,< will coerce null to 0
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(null !== undefined);
+console.log(null >= undefined);
+
+
+
